a506dd3d984ea2e1172717853f96caedb7d0ebe7
mha <mha>
2023-05-07 19:54:55 +0200

    add path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mohamedHassen.github.io',
-    base: '/astroPortfolio',
+    base: '/Portfolio',
 });
